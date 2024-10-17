ba98c922eee86857d05c32c4650ff1929b3cfecb
enryleinhard <42536568+enryleinhard@users.noreply.github.com>
2024-10-18 04:59:46 +0800

    feat: add monthly query

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ import polars as pl
 
 from supabase import create_client
 
+st.set_page_config(
+    page_title="Transaction Data Visualizer", page_icon="💵", layout="wide"
+)
+
 
 @st.cache_resource
 def initialize_supabase_connection():
@@ -13,13 +17,29 @@ supabase = initialize_supabase_connection()
 
 
 @st.cache_data(ttl=60)
-def query_tx_data():
-    return supabase.table(st.secrets["SUPABASE_TX_TABLE_KEY"]).select().execute().data
+def query_tx_data(month):
+    return (
+        supabase.table(st.secrets["SUPABASE_TX_TABLE_KEY"])
+        .select()
+        .like("tx_date", f"%-{month}-%")
+        .execute()
+        .data
+    )
 
 
 def main():
+    st.write(st.session_state)
     st.title("Transaction Data Visualizer")
-    st.write(pl.DataFrame(query_tx_data()))
+    st.selectbox(
+        "Month:",
+        range(1, 13),
+        key="month",
+    )
+    tx_data = query_tx_data(st.session_state.month)
+    if tx_data:
+        st.write(pl.DataFrame(tx_data))
+    else:
+        st.write("No data found for this month")
 
 
 main()
